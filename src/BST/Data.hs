1fe48ed0c9e88ef1e77211c95c8c3596adf2e5e8
Brayan Martínez Santana <brayanmtz@ciencias.unam.mx>
2023-09-20 08:47:59 -0600

    test(Generator): Agrega test para mostrar la mala y correcta definición del generador

--- src/BST/Data.hs
+++ src/BST/Data.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE DeriveGeneric #-}
 
-module BST.Data (BST(Leaf, Branch)) where
+module BST.Data (BST(Leaf, Branch), Tree, Key, Val) where
 
 import GHC.Generics
+
+type Key = Int
+type Val = Int
+type Tree = BST Key Val
 
 data BST k v =
     Leaf |
     Branch (BST k v) k v (BST k v)
     deriving (Eq, Show, Generic)
-
-
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,13 @@
 import Test.Hspec
 
 import qualified Examples.ListSpec
+import qualified BST.Generator.BadGeneratorSpec
+import qualified BST.Generator.CorrectGeneratorSpec
 
 main :: IO ()
 main = hspec $ do
   describe "Examples.ListSpec" Examples.ListSpec.spec
+  describe "BST.DataSpec.Generator.Bad" BST.Generator.BadGeneratorSpec.spec
+  describe "BST.DataSpec.Generator.Correct" BST.Generator.CorrectGeneratorSpec.spec
 
 
